769b4d9e670006ba54bc89116cc6332212bc7b89
liuliangzheng <liuliangzheng520@163.com>
2022-04-12 13:05:13 +0800

    提交JS文件

---
+++ shortUrl.js
@@ -0,0 +1,68 @@
+let res
+  function shorturl() {
+    if(document.querySelector("#text").value==""){
+        alert("Url cannot be empty!")
+        return
+    }
+
+    document.getElementById("searchbtn").disabled=true;
+	document.getElementById("searchbtn").innerHTML='<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>请稍候...';
+    fetch(window.location.pathname, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url: document.querySelector("#text").value })
+    }).then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    res = myJson;
+    document.getElementById("searchbtn").disabled=false;
+	document.getElementById("searchbtn").innerHTML=' 缩短它';
+    if(res.key!=="")
+    document.getElementById("result").innerHTML=window.location.host+res.key;
+    $('#exampleModal').modal('show')
+  }).catch(function(err){alert("未知错误。请重试!");
+  document.getElementById("searchbtn").disabled=false;
+	document.getElementById("searchbtn").innerHTML='缩短它';})
+  }
+  function copyurl (id, attr) {
+    let target = null;
+
+    if (attr) {
+        target = document.createElement('div');
+        target.id = 'tempTarget';
+        target.style.opacity = '0';
+        if (id) {
+            let curNode = document.querySelector('#' + id);
+            target.innerText = curNode[attr];
+        } else {
+            target.innerText = attr;
+        }
+        document.body.appendChild(target);
+    } else {
+        target = document.querySelector('#' + id);
+    }
+
+    try {
+        let range = document.createRange();
+        range.selectNode(target);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand('copy');
+        window.getSelection().removeAllRanges();
+    } catch (e) {
+        console.log('Copy error')
+    }
+
+    if (attr) {
+        // remove temp target
+        target.parentElement.removeChild(target);
+    }
+  }
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+  let notice="短链接用户有效，请勿用于非常跳转，请遵守国家法律法规"
+  if(window.location.host=="5it.me"){
+    document.getElementById("notice").innerHTML=notice
+  }
