d7352a63980c6b4914e6e1486c9d74d535d3f6f1
liuliangzheng <liuliangzheng520@163.com>
2022-04-23 17:14:48 +0800

    bd

---
+++ bd.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?20b76fce632dc35a49456b9e7f8e1032";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
