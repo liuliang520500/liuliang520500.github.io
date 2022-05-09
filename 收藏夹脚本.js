2d8353217095b07b10268427d8bc14f8f94cc0b3
liuliangzheng <liuliangzheng520@163.com>
2022-05-09 18:31:01 +0800

    收藏夹脚本

--- 收藏夹脚本.js
+++ 收藏夹脚本.js
@@ -1,15 +1,1 @@
-var xhr = new XMLHttpRequest();
-xhr.open('POST', 'https://d.glf2ym.cn/');
-xhr.setRequestHeader('Content-type', 'application/json');
-var data = {
-    "url": window.location.href
-};
-xhr.send(JSON.stringify(data));
-xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var res = JSON.parse(xhr.responseText);
-        if (typeof res.key !== 'undefined') {
-            prompt("短网址：", 'https://d.glf2ym.cn'+res.key)
-        }
-    }
-}
+javascript:var xhr=new XMLHttpRequest();xhr.open("POST","https://d.glf2ym.cn/");xhr.setRequestHeader("Content-type","application/json");var data={url:window.location.href};xhr.send(JSON.stringify(data));xhr.onreadystatechange=function(){if(xhr.readyState==4&&xhr.status==200){var res=JSON.parse(xhr.responseText);if(typeof res.key!=="undefined"){prompt("短网址：","https://d.glf2ym.cn"+res.key)}}};
